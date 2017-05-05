e0a3d7a84bdb26aa54d315370dff08d368783dfe
tianpusen <tps@jfpal.com>
2017-05-05 23:23:30 +0800

    添加spring-cloud-eureka模块

---
+++ spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/tian/pusen/web/EurekaClientController.java
@@ -0,0 +1,45 @@
+package tian.pusen.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by tianpusen on 2017/5/5.
+ */
+@RestController
+public class EurekaClientController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/discovery")
+    public String doDiscoveryService(){
+        StringBuilder buf = new StringBuilder();
+        List<String> serviceIds = discoveryClient.getServices();
+        if(!CollectionUtils.isEmpty(serviceIds)){
+            for(String s : serviceIds){
+                System.out.println("serviceId:" + s);
+                List<ServiceInstance> serviceInstances =  discoveryClient.getInstances(s);
+                if(!CollectionUtils.isEmpty(serviceInstances)){
+                    for(ServiceInstance si:serviceInstances){
+                        buf.append("["+si.getServiceId() +" host=" +si.getHost()+" port="+si.getPort()+" uri="+si.getUri()+"]");
+                    }
+                }else{
+                    buf.append("no service.");
+                }
+            }
+        }
+        return buf.toString();
+    }
+
+    @RequestMapping("/eureka-client-test")
+    public String test(){
+        return "eureka-client-test";
+    }
+}
