53d0313385b04032d2323104ac956747e491e9c2
tianpusen <tps@jfpal.com>
2017-05-05 13:30:28 +0800

    spring cloud config

---
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/tian/pusen/web/ConfigClientController.java
@@ -0,0 +1,22 @@
+package tian.pusen.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tianpusen on 2017/5/1.
+ */
+@RestController
+@EnableDiscoveryClient
+
+public class ConfigClientController {
+    @Value("${author-name}") String authorName;
+    @Value("${project.name}") String projectName;
+
+    @RequestMapping("/")
+    public String showAutherName() {
+        return "The author name is: " + authorName +" at the project "+projectName;
+    }
+}
